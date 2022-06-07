6d734224fdf75cf5efc34bc0b95ad84d2fb3b03e
Hitanshi-Sharma <103354645+Hitanshi-Sharma@users.noreply.github.com>
2022-06-07 21:45:03 +0530

    Add files via upload

---
+++ Assignment3 output.py
@@ -0,0 +1,41 @@
+C:\Users\dogra\PycharmProjects\pythonProject\venv\Scripts\python.exe C:/Users/dogra/AppData/Roaming/JetBrains/PyCharmCE2022.1/scratches/scratch_5.py
+Enter the number 6
+0b110
+
+
+Enter the first number:8
+Enter the second number:9
+choose operator (+,-,*,/,%)
+Enter operator :+
+17
+
+
+35
+Enter the number:6
+15.0
+Enter the value:45
+Enter the value:34
+25446.900494077323
+5.716917569364885
+Enter the value:20
+Enter the value:10
+Enter the value:40
+Enter the value:30
+1.0
+
+
+[0, 1, 2, 3, 4]
+[3, 4, 5, 6, 7, 8, 9]
+[4, 7, 10]
+[15, 13, 11, 9, 7]
+[]
+
+
+Enter the number of H atoms in the carbohydrate: 5
+Enter the number of C atoms in the carbohydrate: 6
+Enter the number of O atoms in the carbohydrate:8
+The total weight of carbohydrate is : 205.10070000000002 grams per mole
+
+
+
+Process finished with exit code 0
